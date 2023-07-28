bc1b2ab915545906dc3e3e89b76eed88e11e4cce
Aivenest <aivenest@gmail.com>
2023-07-28 20:04:35 +0500

    ver 1.0

--- modules/web/src/com/company/ordermanagement/web/screens/order/OrderEdit.java
+++ modules/web/src/com/company/ordermanagement/web/screens/order/OrderEdit.java
@@ -4,5 +4,12 @@ import com.haulmont.cuba.gui.components.AbstractEditor;
 import com.company.ordermanagement.entity.Order;
 
 public class OrderEdit extends AbstractEditor<Order> {
-
+@Override
+    protected boolean preCommit(){
+    if(getItem().getOrderDate().after(getItem().getDeliveryDate())){
+        showNotification("something is wrong here:", NotificationType.ERROR);
+        return false;
+    }
+    else return true;
+}
 }
